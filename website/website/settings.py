04a61e4e17b530c0d6168cd264a7beb4c9512b77
mohammed3738 <baigm066@gmail.com>
2024-05-06 14:33:01 +0530

    static settings changed

--- website/website/settings.py
+++ website/website/settings.py
@@ -145,7 +145,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #     BASE_DIR /"static",
 #     ]
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT  = os.path.join(BASE_DIR, 'static')
 
 # # HTTPS settings
